609dd5d69f4ff239d30f656c0e74495382f2c024
PhilipeeX <dev.philipe@outlook.com>
2024-07-30 23:30:41 -0300

    build(rails4): Creates boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4Project::Application.config.secret_key_base = '1f00ca41d1e5839b278f81abef1a77fbe10e7a87b259a5f1cfbd8c6a56f0e91313ae747c2ffe47f5c2d7f5228f8b50c0ece13175001649ab01f26f9e37ff03a3'
